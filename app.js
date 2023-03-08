9a0fd424a4b5305153586fb89e415003ebd35ae4
Iryna Borshchyk <ira011019997@gmail.com>
2023-03-08 23:37:52 +0200

    create counter for cart

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+let productsCountEl = document.querySelector(".order");
+let addToCartBtns = document.querySelectorAll(".btn-add-to-cart");
+
+for(let i = 0; i < addToCartBtns.length; i++) {
+    addToCartBtns[i].addEventListener("click", function() {
+        productsCountEl.textContent = +productsCountEl.textContent + 1; 
+    });
+}
 
