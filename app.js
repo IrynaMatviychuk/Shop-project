4080649f2793401d6a2b4aaaf570a72226671796
Iryna Borshchyk <ira011019997@gmail.com>
2023-03-11 21:10:35 +0200

    Show modal-block while scrolling

--- app.js
+++ app.js
@@ -2,17 +2,52 @@
 let productsCountEl = document.querySelector(".order");
 let addToCartBtns = document.querySelectorAll(".btn-add-to-cart");
 
-for(let i = 0; i < addToCartBtns.length; i++) {
-    addToCartBtns[i].addEventListener("click", function() {
-        productsCountEl.textContent = +productsCountEl.textContent + 1; 
-    });
+for (let i = 0; i < addToCartBtns.length; i++) {
+  addToCartBtns[i].addEventListener("click", function () {
+    productsCountEl.textContent = +productsCountEl.textContent + 1;
+  });
 }
 
 // Add likes
 let likeBtns = document.querySelectorAll(".like");
 
-likeBtns.forEach((item) => 
-    item.addEventListener("click", function () {
-        item.classList.toggle("like-active-btn"); 
-    })
+likeBtns.forEach((item) =>
+  item.addEventListener("click", function () {
+    item.classList.toggle("like-active-btn");
+  })
 );
+
+// Add and open modal-block
+let modal = document.querySelector(".modal");
+let moreDetailsBtns = document.querySelectorAll(".btn-more-details");
+
+moreDetailsBtns.forEach((item) => item.addEventListener("click", openModal));
+function openModal() {
+  modal.classList.add("show");
+  modal.classList.remove("hide");
+}
+
+// Close modal-block (click on btn-close (X))
+let close = document.querySelector(".btn-close");
+
+close.addEventListener("click", closeModal);
+function closeModal() {
+  modal.classList.add("hide");
+  modal.classList.remove("show");
+}
+
+// Close modal (click on modal-block)
+modal.addEventListener("click", function (e) {
+  if (e.target === modal) {
+    closeModal();
+  }
+});
+
+//  Show modal-block while scrolling
+function showModalByScroll() {
+  if (window.scrollY >= document.body.scrollHeight / 2) {
+    openModal();
+    window.removeEventListener("scroll", showModalByScroll);
+  }
+}
+window.addEventListener("scroll", showModalByScroll);
